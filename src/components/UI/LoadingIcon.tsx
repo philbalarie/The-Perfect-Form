cc5babe0ba59290896278094b660bb93ae5a361a
Philippe Alarie <philbalarie@gmail.com>
2023-03-21 16:56:56 -0400

    Change inputs

--- src/components/UI/LoadingIcon.tsx
+++ src/components/UI/LoadingIcon.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const LoadingIcon: React.FC = props => (
+const LoadingIcon: React.FC = () => (
     <svg className="animate-spin -ml-0.5 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg"
          fill="none" viewBox="0 0 24 24">
         <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor"
--- src/components/UI/form/Button.tsx
+++ src/components/UI/form/Button.tsx
@@ -15,7 +15,6 @@ const Button: React.FC<ButtonProps> = props => {
             type="submit"
             className={`inline-flex items-center gap-x-1.5 rounded-md bg-indigo-${opacity} py-2.5 px-3.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-${opacity - 100} focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600`}
         >
-            {/*TODO: Ajout loading icon au loading*/}
             {props.loading ? <LoadingIcon /> : <EnvelopeIcon className="-ml-0.5 h-5 w-5" aria-hidden="true" />}
             Subscribe
         </button>
