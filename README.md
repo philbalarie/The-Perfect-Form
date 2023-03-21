03227a45dd925ea3d7c30c571479f7b54d3e32ac
Philippe Alarie <philbalarie@gmail.com>
2023-03-21 19:33:01 -0400

    Change stuff

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 ## TODO
 - add validation for select value
+- create a toaster to show the success or error validation message after the submit action
+- configure eslint & prettier: https://levelup.gitconnected.com/configure-eslint-and-prettier-for-your-react-project-like-a-pro-2022-10287986a1b6
--- src/services/subscription.service.ts
+++ src/services/subscription.service.ts
@@ -1,6 +1,7 @@
 import {Subscription} from "../types/types";
 import Axios from 'axios'
 
+// TODO: Validate the returned value
 export default class SubscriptionService {
     public static async save(subscription: Subscription) {
         const res = await Axios.post<Subscription>("http://localhost:3004/subscriptions", subscription)
